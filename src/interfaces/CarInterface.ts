ab750211c3a7acedb67c7395c6e24b15a8e6679b
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-11 17:31:27 -0300

    improve logic for zod validations

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -3,8 +3,22 @@ import { z } from 'zod';
 import { Vehicle } from './VehicleInterface';
 
 export const CarSchema = z.object({
-  doorsQty: z.number().int().gte(2).lte(4),
-  seatsQty: z.number().int().gte(2).lte(7),
+  doorsQty: z
+    .number({
+      required_error: 'Doors quantity is required',
+      invalid_type_error: 'Doors quantity must be a number',
+    })
+    .int()
+    .gte(2, { message: 'Doors quantity must be greater or equal than 2' })
+    .lte(4, { message: 'Doors quantity must be less or equal than 4' }),
+  seatsQty: z
+    .number({
+      required_error: 'Seats quantity is required',
+      invalid_type_error: 'Seats quantity must be a number',
+    })
+    .int()
+    .gte(2, { message: 'Seats quantity must be greater or equal than 2' })
+    .lte(7, { message: 'Seats quantity must be less or equal than 7' }),
 });
 
 type CarType = z.infer<typeof CarSchema>;
--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,10 +1,30 @@
 import { z } from 'zod';
 
 const VehicleSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
-  buyValue: z.number().int(),
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, { message: 'Model must be at least 3 characters long' }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .gte(1900, { message: 'Year must be greater or equal than 1900' })
+    .lte(2022, { message: 'Year must be less or equal than 2022' }),
+  color: z
+    .string({
+      required_error: 'Color is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3, { message: 'Color must be at least 3 characters long' }),
+  buyValue: z
+    .number({
+      required_error: 'Buy value is required',
+      invalid_type_error: 'Buy value must be a number',
+    }).int(),
 });
 
 type VehicleType = z.infer<typeof VehicleSchema>;
