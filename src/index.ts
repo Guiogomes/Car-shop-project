97cca75d62d251ceacda004bcd10f718b27cd8bf
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-11 17:50:21 -0300

    implements index for server works

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import App from './app';
+import { Car } from './interfaces/CarInterface';
+import CarsController from './controllers/Cars';
+import GenericRouter from './routes/Routes';
+
+const server = new App();
+const carsController = new CarsController();
+const carsRoute = new GenericRouter<Car>();
+
+carsRoute.addRoutes(carsController);
+server.addRouter(carsRoute.router);
+
+server.startServer();
